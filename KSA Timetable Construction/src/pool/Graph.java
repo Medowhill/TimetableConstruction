402c3c325fff3cac0fa54960ca4c08094892f38d
Jaemin Hong <hjm0901@naver.com>
2015-06-04 14:19:40 +0900

    모든 clique 찾도록 변경

--- KSA Timetable Construction/src/pool/Graph.java
+++ KSA Timetable Construction/src/pool/Graph.java
@@ -1,14 +1,15 @@
 package pool;
 
+import java.util.ArrayList;
+
 public class Graph {
 
 	private int edgeNum = 0;
 
 	private int max;
-	private boolean found;
 	private int[] c;
 	private int[] max_clique;
-	private int[] real_max_clique;
+	private ArrayList<int[]> maxCliques;
 
 	private final boolean[][] edges;
 
@@ -16,6 +17,7 @@ public class Graph {
 		edges = new boolean[vertices][vertices];
 		c = new int[vertices];
 		max_clique = new int[vertices];
+		maxCliques = new ArrayList<>();
 	}
 
 	public boolean addEdge(int i, int j) {
@@ -38,21 +40,27 @@ public class Graph {
 		if (vertices.length == 0) {
 			if (size > max) {
 				max = size;
-				real_max_clique = new int[max];
+				int[] tmp = new int[max];
 				for (int i = 0; i < max; i++)
-					real_max_clique[i] = max_clique[i];
-				found = true;
+					tmp[i] = max_clique[i];
+				maxCliques.clear();
+				maxCliques.add(tmp);
+			} else if (size == max) {
+				int[] tmp = new int[max];
+				for (int i = 0; i < max; i++)
+					tmp[i] = max_clique[i];
+				maxCliques.add(tmp);
 			}
 			return;
 		}
 
 		while (vertices.length != 0) {
-			if (size + vertices.length <= max)
+			if (size + vertices.length < max)
 				return;
 
 			int min = vertices[0];
 
-			if (size + c[min] <= max)
+			if (size + c[min] < max)
 				return;
 
 			vertices = remove(vertices, min);
@@ -60,13 +68,10 @@ public class Graph {
 			max_clique[size] = min;
 
 			clique(adjacent(vertices, min), size + 1);
-
-			if (found)
-				return;
 		}
 	}
 
-	public int[] maxClique(boolean ordering) {
+	public ArrayList<int[]> maxClique(boolean ordering) {
 		max = 0;
 
 		int[] arr;
@@ -79,7 +84,6 @@ public class Graph {
 		}
 
 		for (int i = edges.length - 1; i >= 0; i--) {
-			found = false;
 
 			int k = 0;
 			int[] tmpVertices = new int[edges.length - i];
@@ -101,7 +105,7 @@ public class Graph {
 			c[arr[i]] = max;
 		}
 
-		return real_max_clique;
+		return maxCliques;
 	}
 
 	private int[] ordering() {
