3b71f6d4f1fa65cfa7f504a394e8bfc4a61113f6
Jaemin <hjm0901@naver.com>
2015-07-05 22:34:03 +0900

    학생 배치 그리디 알고리즘 Course 수정

--- KSA Timetable Construction/src/pool/Course.java
+++ KSA Timetable Construction/src/pool/Course.java
@@ -10,76 +10,28 @@ public class Course {
     // 과목명
     public final String name;
 
+    // 과목의 분반 리스트
+    public final DivideClass[] classes;
+
     // 과목에 속한 분반의 수
-    private int classNumber;
+    private final int classNumber;
 
     // 과목을 수강 신청한 학생의 수
-    private int studentNumber;
+    private final int studentNumber;
 
     // 과목의 수업 시수 구성
-    private int timeComposition;
-
-    // 남은 분반에 배정할 학생의 수
-    private int remainStudentNumber;
-
-    // 현재 학생을 배정중인 분반
-    private DivideClass assigningClass;
-
-    // 현재까지 만든 분반의 수
-    private int madeClass;
+    private final int timeComposition;
 
     // 생성자
-    public Course(String name) {
+    public Course(String name, int classNumber, int studentNumber, int timeComposition) {
         this.name = name;
-    }
-
-    // 현재 학생을 배정 중인 분반을 return
-    public DivideClass getAssigningClass() {
-        if (assigningClass == null)
-            assigningClass = new DivideClass(this, ++madeClass,
-                    remainStudentNumber / (classNumber - madeClass + 1) + ((remainStudentNumber % (classNumber - madeClass + 1) != 0) ? 1 : 0),
-                    timeComposition);
-        return assigningClass;
-    }
-
-    public int getBalance() {
-        return assigningClass.getBalance();
-    }
-
-    public void finishCurrentAssigning() {
-        remainStudentNumber -= assigningClass.getStudentNumber();
-        assigningClass = null;
-    }
-
-    // 분반 수 getter
-    public int getClassNumber() {
-        return classNumber;
-    }
-
-    // 분반 수 setter
-    public void setClassNumber(int classNumber) {
         this.classNumber = classNumber;
-    }
-
-    // 학생 수 getter
-    public int getStudentNumber() {
-        return studentNumber;
-    }
-
-    // 학생 수 setter
-    public void setStudentNumber(int studentNumber) {
         this.studentNumber = studentNumber;
-        remainStudentNumber = studentNumber;
-    }
-
-    // 수업 시수 구성 getter
-    public int getTimeComposition() {
-        return timeComposition;
-    }
-
-    // 수업 시수 구성 setter
-    public void setTimeComposition(int timeComposition) {
         this.timeComposition = timeComposition;
+
+        classes = new DivideClass[classNumber];
+        for (int i = 0; i < classNumber; i++)
+            classes[i] = new DivideClass(this, i + 1, studentNumber / classNumber, timeComposition);
     }
 
     @Override
